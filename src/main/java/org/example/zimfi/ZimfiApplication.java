934af0510f5ed84d98791a453639948c1281f95c
Caston mubvumbi <castonmubvumbi@gmail.com>
2025-06-17 13:00:05 +0200

    intial commit : microfinanceapp zimfi

---
+++ src/main/java/org/example/zimfi/ZimfiApplication.java
@@ -0,0 +1,13 @@
+package org.example.zimfi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZimfiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZimfiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/zimfi/ZimfiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.zimfi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZimfiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
